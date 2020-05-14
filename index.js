6f85470ad1370eb04d25db078cc5695c1609c7f4
phamthai12316 <phamthai12316@gmail.com>
2020-05-14 17:09:43 +0700

    CodersX-Main-Lession

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var low = require('lowdb')
-var FileSync = require('lowdb/adapters/FileSync')
+var low = require('lowdb');
+var shortid = require('shortid');
+var FileSync = require('lowdb/adapters/FileSync');
 
-var adapter = new FileSync('db.json')
-var db = low(adapter)
+var adapter = new FileSync('db.json');
+var db = low(adapter);
 
 // Set some defaults (required if your JSON file is empty)
 db.defaults({ users: [] })
@@ -40,8 +41,6 @@ app.get('/users/search', function(req,res){
   var matchedUsers =  db.get('users').value().filter(function(user){
     return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
   });
-  console.log(matchedUsers)
-
   res.render('users/index',{
     users: matchedUsers
   })
@@ -52,8 +51,38 @@ app.get('/users/create', function(req,res){
 })
 
 app.post('/users/create',function(req,res){
+  req.body.id = shortid.generate();
   db.get('users').push(req.body).write();
   res.redirect('/users')
+})
+
+app.get('/users/:id',function(req,res){
+  var id = req.params.id;
+  var user = db.get('users').find({id : id}).value();
+  res.render('users/view',{
+    users: user
+  })
+})
+
+app.get('/users/:id/delete', function(req,res){
+  var id = req.params.id;
+  db.get('users').remove({id: id}).write();
+  res.redirect('/users');
+})
+
+
+app.get('/users/:id/update', function(req,res){
+  var id = req.params.id;
+  var user = db.get('users').find({id : id}).value();
+  res.render('users/update',{
+    users:user
+  })
+})
+
+app.post('/users/:id/update', function(req,res){
+  var id = req.params.id;
+  var user = db.get('users').find({id : id}).assign({name : req.body.name}).write();
+  res.redirect('/users');
 })
 
 app.listen(port, function(){
